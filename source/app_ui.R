18ef2c513bcbfb97d791b35f697940cd97b28095
SheaKim <sheakim2@uw.edu>
2022-12-08 23:57:16 -0800

    library changed

--- source/app_ui.R
+++ source/app_ui.R
@@ -1,15 +1,6 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(plotly)
 library(shiny)
 library(shinythemes)
-library(plotly)
 
 source("app_server.R")
 
